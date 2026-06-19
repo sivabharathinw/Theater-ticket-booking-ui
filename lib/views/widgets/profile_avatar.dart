2581ca2b9a563de88fcb431b63f9ab74ec553391
sivabharathinw <sivabarathi.m@nativewit.in>
2026-06-19 14:46:27 +0530

    CU-BIOMETRICA-186
    refactor:make the profile center by adjustng its padding

--- lib/views/widgets/profile_avatar.dart
+++ lib/views/widgets/profile_avatar.dart
@@ -4,20 +4,18 @@ class ProfileAvatar extends StatelessWidget {
   final String? imageUrl;
   final double size;
 
-  const ProfileAvatar({super.key,
-    this.imageUrl,
-    this.size = 40.0});
+  const ProfileAvatar({super.key, this.imageUrl, this.size = 40.0});
 
   @override
   Widget build(BuildContext context) {
     return
-    Padding(
-      padding:EdgeInsets.only(top:20),
-     child: CircleAvatar(
+      Padding(
+        padding:EdgeInsets.only(top: 16),
+      child:CircleAvatar(
       radius: size / 2,
-      backgroundImage: NetworkImage(imageUrl?? "https://www.profilepicture.ai/free-pfp-maker"),
-
+      backgroundImage: NetworkImage(
+        imageUrl ?? "https://www.profilepicture.ai/free-pfp-maker",
       ),
-    );
+          ),);
   }
 }
